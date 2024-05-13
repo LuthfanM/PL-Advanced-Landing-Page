62c30e4cbd785893758416886594f0db85f7a84f
Luthfan <mursyidanluthfan@gmail.com>
2024-05-13 21:17:36 +0700

    fix vercel problem #2

--- handlers/services/Surfer.ts
+++ handlers/services/Surfer.ts
@@ -1,21 +1,30 @@
-import axiosInstance from "../api"
+import { Surfer } from "@/types/surfer";
+import axiosInstance from "../api";
+import { AxiosResponse } from "axios";
 
-export const getSurfers = async () => {
-    return await axiosInstance.get('/api/surfers');
-}
+export const getSurfers = async (): Promise<AxiosResponse<Surfer[]>> => {
+  return await axiosInstance.get<Surfer[]>("/api/surfers");
+};
 
-export const getSurfer = async (id) => {
-    return await axiosInstance.get(`/api/surfers/${id}`);
-}
+export const getSurfer = async (id: number): Promise<AxiosResponse<Surfer>> => {
+  return await axiosInstance.get<Surfer>(`/api/surfers/${id}`);
+};
 
-export const createSurfer = async (data) => {
-    return await axiosInstance.post('/api/surfers', data);
-}
+export const createSurfer = async (
+  data: Surfer
+): Promise<AxiosResponse<Surfer>> => {
+  return await axiosInstance.post<Surfer>("/api/surfers", data);
+};
 
-export const updateSurfer = async (id, data) => {
-    return await axiosInstance.put(`/api/surfers/${id}`, data);
-}
+export const updateSurfer = async (
+  id: number,
+  data: Partial<Surfer>
+): Promise<AxiosResponse<Surfer>> => {
+  return await axiosInstance.put<Surfer>(`/api/surfers/${id}`, data);
+};
 
-export const deleteSurfer = async (id) => {
-    return await axiosInstance.delete(`/api/surfers/${id}`);
-}
+export const deleteSurfer = async (
+  id: number
+): Promise<AxiosResponse<void>> => {
+  return await axiosInstance.delete<void>(`/api/surfers/${id}`);
+};
---
+++ types/surfer.ts
@@ -0,0 +1,10 @@
+export interface Surfer {
+    name: string | undefined;
+    country: string | undefined;
+    email: string | undefined;
+    phone_number: string | undefined;
+    id_card: File | null;  
+    visit_date: string | undefined;
+    experience: string | undefined;
+    desired_board: string | undefined;
+  }
